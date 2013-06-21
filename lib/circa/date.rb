131788d8f3becce7f56717074a420b97b61ad759
Robert Dallas Gray <robert.dallas.gray@huzutech.com>
2013-06-21 11:58:57 +0100

    Passing tests with valid_parts.

--- lib/circa/date.rb
+++ lib/circa/date.rb
@@ -4,11 +4,16 @@ module Circa
       @year = "0000"
       @month = "00"
       @day = "00"
+      @valid_parts = {}
       raise ArgumentError unless validate(date_string)
     end
 
     def to_s
       "#{@year}-#{@month}-#{@day}"
+    end
+
+    def valid_parts
+      @valid_parts
     end
 
     private
@@ -20,14 +25,19 @@ module Circa
       @year = matches[1]
       if @year.to_i == 0
         return false if matches[2].to_i > 0
+      else
+        @valid_parts[:year] = @year
       end
       @month = matches[2]
       if @month.to_i == 0
         return false if matches[3].to_i > 0
+      else
+        @valid_parts[:month] = @month
       end
       @day = matches[3]
       if @day.to_i > 0
-        return ::Date.strptime(date_string).to_s == date_string
+        return false unless ::Date.strptime(date_string).to_s == date_string
+        @valid_parts[:day] = @day
       end
       true
     end
--- spec/date_spec.rb
+++ spec/date_spec.rb
@@ -11,11 +11,25 @@ describe Circa::Date do
     lambda { Circa::Date.new("2001-00-11") }.must_raise ArgumentError
   end
 
+  it "should reject an incorrect date" do
+    lambda { Circa::Date.new("2001-02-30") }.must_raise ArgumentError
+  end
+
   it "should return a correct date string intact" do
     Circa::Date.new("2001-11-11").to_s.must_equal "2001-11-11"
   end
 
   it "should return a correct partial date string" do
     Circa::Date.new("2001-11-00").to_s.must_equal "2001-11-00"
+  end
+
+  it "should return valid date parts for a complete date" do
+    parts = Circa::Date.new("2001-11-01").valid_parts
+    parts.must_equal({ :year => "2001", :month => "11", :day => "01" })
+  end
+
+  it "should return valid date parts for a partial date" do
+    parts = Circa::Date.new("2001-11-00").valid_parts
+    parts.must_equal({ :year => "2001", :month => "11" })
   end
 end
